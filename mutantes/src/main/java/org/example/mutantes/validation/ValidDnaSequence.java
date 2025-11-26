3b19c8888f1e62de6b4e55381afd88f1724a7c00
12juangallardo-wq <12juangallardo@gmail.com>
2025-11-26 20:06:22 -0300

    Add files via upload

---
+++ mutantes/src/main/java/org/example/mutantes/validation/ValidDnaSequence.java
@@ -0,0 +1,21 @@
+package org.example.mutantes.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = ValidDnaSequenceValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidDnaSequence {
+
+    String message() default "Invalid DNA sequence: must be a square NxN matrix (minimum 4x4) with only A, T, C, G characters";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
+
+
